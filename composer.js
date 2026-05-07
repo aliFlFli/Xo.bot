38babb26d0249e48373b7c80683141df2d92ed41
@ShotifyBot <aliflflin@gmail.com>
2026-05-07 21:37:16 +0330

    Create composer.js

---
+++ composer.js
@@ -0,0 +1,16 @@
+{
+  "name": "minesweeper-bale",
+  "version": "6.3.0",
+  "description": "Minesweeper PRO Bot for Bale Messenger",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "node-bale-sdk": "^1.0.0",
+    "express": "^4.18.2",
+    "better-sqlite3": "^9.0.0",
+    "dotenv": "^16.3.1",
+    "crypto": "^1.0.1"
+  }
+}
